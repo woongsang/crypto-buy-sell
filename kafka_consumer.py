be2f77a4104a94d31c38ef785c4f99ce224e6f0c
Woongsup Jung (정웅섭) <woong@lunit.io>
2022-03-24 14:15:41 +0900

    Add backtesting option

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -8,8 +8,9 @@ from dotenv import dotenv_values
 
 @background
 def consume_data(market):
+    topic = market
     config = dotenv_values(".env")
-    consumer = KafkaConsumer(market,
+    consumer = KafkaConsumer(topic,
                              bootstrap_servers=[f"{config['KAFKA_HOST']}:{config['KAFKA_PORT']}"],
                              auto_offset_reset='latest',
                              enable_auto_commit=True,
@@ -20,8 +21,28 @@ def consume_data(market):
         run_sessions(sessions_db, market, data)
 
 
+def consume_back_testing_data(market):
+    topic = market + '_B'
+    config = dotenv_values(".env")
+    consumer = KafkaConsumer(topic,
+                             bootstrap_servers=[f"{config['KAFKA_HOST']}:{config['KAFKA_PORT']}"],
+                             auto_offset_reset='latest',
+                             enable_auto_commit=True,
+                             )
+    sessions_db = db_connection('larry_sessions_bt')
+    for message in consumer:
+        data = json.loads(message.value)
+        run_sessions(sessions_db, market, data, bt=True)
+
+
 def start_consuming():
     config = dotenv_values(".env")
     markets = config['SUBSCRIPTION_LIST'].split(',')
     for market in markets:
-        consume_data(market.upper())
+        market = market.upper()
+        if bool(config['BACK_TEST']):
+            consume_back_testing_data(market)
+        else:
+            consume_data(market)
+
+
